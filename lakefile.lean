20aa77f48b2a0f86973584816ae4a7320497bf60
Anthony William Moruzin <antneytwin@gmail.com>
2026-06-20 06:40:35 +0000

    Add srcDir to lean_lib so modules resolve from root

--- lakefile.lean
+++ lakefile.lean
@@ -7,4 +7,5 @@ package "my_project" where
 require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "v4.31.0"
 
 @[default_target]
-lean_lib AciLib
+lean_lib AciLib where
+  srcDir := "."
