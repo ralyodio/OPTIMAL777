7c6aa3f400e03e506988a48aed33bc0d5fc29975
Anthony William Moruzin <antneytwin@gmail.com>
2026-06-20 05:56:19 +0000

    Restore lakefile glob and toolchain v4.31.0-rc2

--- lakefile.lean
+++ lakefile.lean
@@ -4,8 +4,8 @@ open Lake DSL
 package "my_project" where
   srcDir := "."
 
-require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "v4.31.0"
+require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "v4.31.0-rc2"
 
 @[default_target]
-lean_lib "MyProject" where
-  roots := #[ `ACIManifold, `Governor, `ACI_Governance.Root, `ACI_Governance.Node_2, `ACI_System_Kernel ]
+lean_lib AciLib where
+  globs := #[.andSubmodules `AciLib]
